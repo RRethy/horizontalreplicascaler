b99287ba42840aa07fb00f024f75444ceaa5c9c6
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2024-07-01 23:03:31 -0400

    Expand on the tests so they're ready to test real stuff

--- internal/controller/horizontalreplicascaler_controller_test.go
+++ internal/controller/horizontalreplicascaler_controller_test.go
@@ -2,51 +2,116 @@ package controller
 
 import (
 	"context"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 
 	rrethyv1 "github.com/RRethy/horizontalrpelicascaler/api/v1"
 )
 
+const (
+	timeout        = time.Second * 20
+	interval       = time.Millisecond * 250
+	scalerName     = "test-scaler"
+	namespace      = "default"
+	deploymentName = "test-deployment"
+)
+
+var (
+	defaultScalerNamespacedName = types.NamespacedName{
+		Name:      scalerName,
+		Namespace: namespace,
+	}
+	defaultDeployment = &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deploymentName,
+			Namespace: namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: ptr.To[int32](3),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "test",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "test",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:    "busybox",
+							Image:   "busybox",
+							Command: []string{"sleep", "3600"},
+						},
+					},
+				},
+			},
+		},
+	}
+	defaultHorizontalReplicaScaler = &rrethyv1.HorizontalReplicaScaler{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      scalerName,
+			Namespace: namespace,
+		},
+		Spec: rrethyv1.HorizontalReplicaScalerSpec{
+			ScaleTargetRef: &rrethyv1.ScaleTargetRef{
+				Group: "apps",
+				Kind:  "Deployment",
+				Name:  deploymentName,
+			},
+			MinReplicas: 3,
+			MaxReplicas: 10,
+			Metrics: []rrethyv1.MetricSpec{
+				{
+					Type: "static",
+					Target: rrethyv1.TargetSec{
+						Type:  "value",
+						Value: "10",
+					},
+				},
+			},
+		},
+	}
+)
+
 var _ = Describe("HorizontalReplicaScaler Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
-		const namespace = "default"
+	Context("When scaling a Deployment", func() {
+		const ()
 
 		ctx := context.Background()
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: namespace,
-		}
-		horizontalreplicascaler := &rrethyv1.HorizontalReplicaScaler{}
-
 		BeforeEach(func() {
-			By("creating the custom resource for the Kind HorizontalReplicaScaler")
-			err := k8sClient.Get(ctx, typeNamespacedName, horizontalreplicascaler)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &rrethyv1.HorizontalReplicaScaler{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
-					},
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
+			By("Creating a default deployment to scale")
+			Expect(k8sClient.Create(ctx, defaultDeployment)).To(Succeed())
+
+			By("Creating a new custom resource for the Kind HorizontalReplicaScaler")
+			Expect(k8sClient.Create(ctx, defaultHorizontalReplicaScaler)).To(Succeed())
 		})
 
 		AfterEach(func() {
-			resource := &rrethyv1.HorizontalReplicaScaler{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
+			By("Cleanup the scaler")
+			Expect(k8sClient.Delete(ctx, defaultHorizontalReplicaScaler)).To(Succeed())
 
-			By("Cleanup the specific resource instance HorizontalReplicaScaler")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+			By("Getting the existing deployment")
+			deployment := &appsv1.Deployment{}
+			err := k8sClient.Get(ctx, types.NamespacedName{Name: deploymentName, Namespace: namespace}, deployment)
+			Expect(err).To(SatisfyAny(BeNil(), WithTransform(errors.IsNotFound, BeTrue())))
+
+			if err == nil {
+				By("Cleanup the deployment")
+				Expect(k8sClient.Delete(ctx, deployment)).To(Succeed())
+			}
 		})
 	})
 })
