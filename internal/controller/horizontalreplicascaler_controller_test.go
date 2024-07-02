7951f2eca736f9be0852a1c4499af2cf18a2f93e
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2024-07-02 13:01:43 -0400

    Add test for taking max of multi metrics

--- internal/controller/horizontalreplicascaler_controller_test.go
+++ internal/controller/horizontalreplicascaler_controller_test.go
@@ -31,75 +31,39 @@ var (
 		Namespace: namespace,
 	}
 	defaultDeployment = &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      deploymentName,
-			Namespace: namespace,
-		},
+		ObjectMeta: metav1.ObjectMeta{Name: deploymentName, Namespace: namespace},
 		Spec: appsv1.DeploymentSpec{
 			Replicas: ptr.To[int32](3),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": "test",
-				},
-			},
+			Selector: &metav1.LabelSelector{MatchLabels: map[string]string{"app": "test"}},
 			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "test",
-					},
-				},
+				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"app": "test"}},
 				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:    "busybox",
-							Image:   "busybox",
-							Command: []string{"sleep", "3600"},
-						},
-					},
+					Containers: []corev1.Container{{Name: "busybox", Image: "busybox", Command: []string{"sleep", "3600"}}},
 				},
 			},
 		},
 	}
 	defaultHorizontalReplicaScaler = &rrethyv1.HorizontalReplicaScaler{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      scalerName,
-			Namespace: namespace,
-		},
+		ObjectMeta: metav1.ObjectMeta{Name: scalerName, Namespace: namespace},
 		Spec: rrethyv1.HorizontalReplicaScalerSpec{
-			ScaleTargetRef: &rrethyv1.ScaleTargetRef{
-				Group: "apps",
-				Kind:  "Deployment",
-				Name:  deploymentName,
-			},
-			MinReplicas: 3,
-			MaxReplicas: 10,
-			Metrics: []rrethyv1.MetricSpec{
-				{
-					Type: "static",
-					Target: rrethyv1.TargetSec{
-						Type:  "value",
-						Value: "10",
-					},
-				},
-			},
+			ScaleTargetRef: &rrethyv1.ScaleTargetRef{Group: "apps", Kind: "Deployment", Name: deploymentName},
+			MinReplicas:    3,
+			MaxReplicas:    10,
+			Metrics:        []rrethyv1.MetricSpec{{Type: "static", Target: rrethyv1.TargetSec{Type: "value", Value: "10"}}},
 		},
 	}
 )
 
 var _ = Describe("HorizontalReplicaScaler Controller", func() {
 	Context("When scaling a Deployment", func() {
-		const ()
-
 		ctx := context.Background()
 
 		BeforeEach(func() {
 			By("Creating a default deployment to scale")
-			deployment := defaultDeployment.DeepCopy()
-			Expect(k8sClient.Create(ctx, deployment)).To(Succeed())
+			Expect(k8sClient.Create(ctx, defaultDeployment.DeepCopy())).To(Succeed())
 
 			By("Creating a new custom resource for the Kind HorizontalReplicaScaler")
-			scaler := defaultHorizontalReplicaScaler.DeepCopy()
-			Expect(k8sClient.Create(ctx, scaler)).To(Succeed())
+			Expect(k8sClient.Create(ctx, defaultHorizontalReplicaScaler.DeepCopy())).To(Succeed())
 		})
 
 		AfterEach(func() {
@@ -145,6 +109,27 @@ var _ = Describe("HorizontalReplicaScaler Controller", func() {
 
 			By("Checking if the event was recorded")
 			Eventually(eventRecorder.Events).Should(Receive(ContainSubstring(fmt.Sprintf("\"%s\" not found", nonExistentDeploymentName))))
+		})
+
+		It("Should take the max of the metrics", func() {
+			By("Getting the existing scaler")
+			var horizontalreplicascaler rrethyv1.HorizontalReplicaScaler
+			Expect(k8sClient.Get(ctx, defaultScalerNamespacedName, &horizontalreplicascaler)).To(Succeed())
+
+			By("Adding a new metric to the scaler")
+			horizontalreplicascaler.Spec.Metrics = []rrethyv1.MetricSpec{
+				{Type: "static", Target: rrethyv1.TargetSec{Type: "value", Value: "9"}},
+				{Type: "static", Target: rrethyv1.TargetSec{Type: "value", Value: "7"}},
+			}
+			Expect(k8sClient.Update(ctx, &horizontalreplicascaler)).To(Succeed())
+
+			By("Getting the deployment to check the replica count")
+			Eventually(func() int32 {
+				var deployment appsv1.Deployment
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: deploymentName, Namespace: namespace}, &deployment)
+				Expect(err).ToNot(HaveOccurred())
+				return *deployment.Spec.Replicas
+			}, timeout, interval).Should(Equal(int32(9)))
 		})
 	})
 })
